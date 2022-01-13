e0cde371553088d59ee24364ebe49ad5a171d516
Naveen Mangla <nmangla@umd.edu>
2022-01-13 00:02:38 -0500

    almost complete

--- src/filter_image.c
+++ src/filter_image.c
@@ -62,10 +62,17 @@ float apply_filter(image im, image filter, int j, int i, int channel) {
 image convolve_image(image im, image filter, int preserve)
 {
     float val;
-    image blur = make_image(im.w, im.h, im.c);
-    for (int channel = 0;channel < im.c;channel++) {
-        for (int i = 0;i < im.h;i++) {
-            for (int j = 0;j < im.w;j++) {
+    image blur;
+    if (preserve == 1) {
+        blur = make_image(im.w, im.h, im.c);
+    }
+    else {
+        blur = make_image(im.w, im.h, 1);
+
+    }
+    for (int channel = 0;channel < blur.c;channel++) {
+        for (int i = 0;i < blur.h;i++) {
+            for (int j = 0;j < blur.w;j++) {
                 val = apply_filter(im, filter, j, i, channel);
                 set_pixel(blur, j, i, channel, val);
             }
@@ -102,31 +109,27 @@ image make_highpass_filter()
 
 image make_sharpen_filter()
 {
-    image highpass = make_image(3, 3, 1);
+    image sharp = make_image(3, 3, 1);
 
     // Row 1
-    set_pixel(highpass, 0, 0, 0, 0);
-    set_pixel(highpass, 1, 0, 0, -1);
-    set_pixel(highpass, 2, 0, 0, 0);
+    set_pixel(sharp, 0, 0, 0, 0);
+    set_pixel(sharp, 1, 0, 0, -1);
+    set_pixel(sharp, 2, 0, 0, 0);
     // Row 2
-    set_pixel(highpass, 0, 1, 0, -1);
-    set_pixel(highpass, 1, 1, 0, 5);
-    set_pixel(highpass, 2, 1, 0, -1);
+    set_pixel(sharp, 0, 1, 0, -1);
+    set_pixel(sharp, 1, 1, 0, 5);
+    set_pixel(sharp, 2, 1, 0, -1);
     // Row 3
-    set_pixel(highpass, 0, 2, 0, 0);
-    set_pixel(highpass, 1, 2, 0, -1);
-    set_pixel(highpass, 2, 2, 0, 0);
+    set_pixel(sharp, 0, 2, 0, 0);
+    set_pixel(sharp, 1, 2, 0, -1);
+    set_pixel(sharp, 2, 2, 0, 0);
 
-
-
-    return highpass;
-    // TODO
-    // return make_image(1,1,1);
+    return sharp;
 }
 
 image make_emboss_filter()
 {
-    // TODO  image highpass = make_image(3, 3, 1);
+    // TODO  
     image highpass = make_image(3, 3, 1);
 
     // Row 1
@@ -175,71 +178,172 @@ image make_gaussian_filter(float sigma)
 
 image add_image(image a, image b)
 {
-    // TODO
-    return make_image(1, 1, 1);
+    assert(a.h == b.h && a.w == b.w && a.c == b.c);
+    image new_image = make_image(a.w, a.h, a.c);
+    float value;
+    for (int c = 0;c < a.c;c++) {
+        for (int i = 0;i < a.h;i++) {
+            for (int j = 0;j < a.w;j++) {
+                value = get_pixel(a, j, i, c) + get_pixel(b, j, i, c);
+                set_pixel(new_image, j, i, c, value);
+
+            }
+        }
+    }
+    return new_image;
 }
 
 image sub_image(image a, image b)
 {
-    // TODO
-    return make_image(1, 1, 1);
+    assert(a.h == b.h && a.w == b.w && a.c == b.c);
+    image new_image = make_image(a.w, a.h, a.c);
+    float value;
+    for (int c = 0;c < a.c;c++) {
+        for (int i = 0;i < a.h;i++) {
+            for (int j = 0;j < a.w;j++) {
+                value = get_pixel(a, j, i, c) - get_pixel(b, j, i, c);
+                set_pixel(new_image, j, i, c, value);
+
+            }
+        }
+    }
+    return new_image;
 }
 
 image make_gx_filter()
 {
-    // TODO
-    return make_image(1, 1, 1);
+    image g_x = make_image(3, 3, 1);
+    set_pixel(g_x, 0, 0, 0, 1);
+    set_pixel(g_x, 1, 0, 0, 0);
+    set_pixel(g_x, 2, 0, 0, -1);
+    // Row 2
+    set_pixel(g_x, 0, 1, 0, 2);
+    set_pixel(g_x, 1, 1, 0, 0);
+    set_pixel(g_x, 2, 1, 0, -2);
+    // Row 3
+    set_pixel(g_x, 0, 2, 0, 1);
+    set_pixel(g_x, 1, 2, 0, 0);
+    set_pixel(g_x, 2, 2, 0, -1);
+    return g_x;
 }
 
 image make_gy_filter()
 {
-    // TODO
-    return make_image(1, 1, 1);
+    image g_y = make_image(3, 3, 1);
+    set_pixel(g_y, 0, 0, 0, 1);
+    set_pixel(g_y, 1, 0, 0, 2);
+    set_pixel(g_y, 2, 0, 0, 1);
+    // Row 2
+    set_pixel(g_y, 0, 1, 0, 0);
+    set_pixel(g_y, 1, 1, 0, 0);
+    set_pixel(g_y, 2, 1, 0, 0);
+    // Row 3
+    set_pixel(g_y, 0, 2, 0, -1);
+    set_pixel(g_y, 1, 2, 0, -2);
+    set_pixel(g_y, 2, 2, 0, -1);
+    return g_y;
+}
+
+float three_way_maximum(float a, float b, float c)
+{
+    return (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+}
+
+float three_way_minimum(float a, float b, float c)
+{
+    return (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
 }
 
 void feature_normalize(image im)
 {
+    float min_r = get_pixel(im, 0, 0, 0);
+    float min_g = get_pixel(im, 0, 0, 1);
+    float min_b = get_pixel(im, 0, 0, 2);
 
-    for (int c = 0;c < im.c;c++) {
-        float min = 0;
-        float max = 0;
-        float value = 0;
-        for (int i = 0;i < im.h;i++) {
-            for (int j = 0;j < im.w;j++) {
+    float max_r = get_pixel(im, 0, 0, 0);
+    float max_g = get_pixel(im, 0, 0, 1);
+    float max_b = get_pixel(im, 0, 0, 2);
 
-                min = (min <= get_pixel(im, j, i, c)) ? min : get_pixel(im, j, i, c);
-                max = (max >= get_pixel(im, j, i, c)) ? min : get_pixel(im, j, i, c);
-            }
+    float r_value, g_value, b_value;
+    for (int row = 0; row < im.h; row++) {
+        for (int col = 0; col < im.w; col++) {
+            r_value = get_pixel(im, col, row, 0);
+            g_value = get_pixel(im, col, row, 1);
+            b_value = get_pixel(im, col, row, 2);
+
+            min_r = (r_value <= min_r) ? r_value : min_r;
+            min_g = (g_value <= min_g) ? g_value : min_g;
+            min_b = (b_value <= min_b) ? b_value : min_b;
+
+            max_r = (r_value >= max_r) ? r_value : max_r;
+            max_g = (g_value >= max_g) ? g_value : max_g;
+            max_b = (b_value >= max_b) ? b_value : max_b;
+
         }
-    
-        for (int i = 0;i < im.h;i++) {
-            for (int j = 0;j < im.w;j++) {
-                value = (max - min == 0) ? 0 : (get_pixel(im, j, i, c) - min) / (max - min);
-                set_pixel(im, j, i, c, value);
+    }
+    float min = three_way_minimum(min_r, min_b, min_g);
+    float max = three_way_maximum(max_b, max_g, max_r);
+    float value;
+    for (int channel = 0;channel < im.c;channel++) {
+        for (int row = 0; row < im.h; row++) {
+            for (int col = 0; col < im.w; col++) {
+                value = (max - min == 0) ? 0 : (get_pixel(im, col, row, channel) - min) / (max - min);
+                set_pixel(im, col, row, channel, value);
             }
         }
     }
 }
 
-//     for (int i = 0;i < im.h;i++) {
-//         for (int j = 0;j < im.w;j++) {
-//             float v;
-//             v = get_pixel(im, j, i, c) / sum;
-//             set_pixel(im, j, i, c, v);
-//         }
-
-//     }
-// }
-
-
 image* sobel_image(image im)
 {
-    // TODO
-    return calloc(2, sizeof(image));
+    image* sobel = calloc(2, sizeof(im));
+
+    image g_x = make_gx_filter();
+    image g_y = make_gy_filter();
+    float val, angle;
+    image g_x_image = convolve_image(im, g_x, 0);
+    image g_y_image = convolve_image(im, g_y, 0);
+
+    image mag = make_image(im.w, im.h, im.c);
+    image ang = make_image(im.w, im.h, im.c);
+
+    for (int c = 0; c < mag.c; c++) {
+        for (int h = 0; h < mag.h; h++) {
+            for (int w = 0; w < mag.w; w++) {
+                val = sqrt(get_pixel(g_x_image, w, h, 0) * get_pixel(g_x_image, w, h, 0) + get_pixel(g_y_image, w, h, 0) * get_pixel(g_y_image, w, h, 0));
+                angle = atan(get_pixel(g_y_image, w, h, 0) / get_pixel(g_x_image, w, h, 0));
+                set_pixel(mag, w, h, c, val);
+                set_pixel(ang, w, h, c, angle);
+
+            }
+        }
+    }
+    sobel[0] = mag;
+    sobel[1] = ang;
+    return sobel;
 }
 
 image colorize_sobel(image im)
 {
-    // TODO
-    return make_image(1, 1, 1);
+    image g_x = make_gx_filter();
+    image g_y = make_gy_filter();
+    float val, angle;
+    image g_x_image = convolve_image(im, g_x, 1);
+    image g_y_image = convolve_image(im, g_y, 1);
+
+    image mag = make_image(im.w, im.h, im.c);
+    image ang = make_image(im.w, im.h, im.c);
+
+    for (int c = 0; c < mag.c; c++) {
+        for (int h = 0; h < mag.h; h++) {
+            for (int w = 0; w < mag.w; w++) {
+                val = sqrt(get_pixel(g_x_image, w, h, c) * get_pixel(g_x_image, w, h, c) + get_pixel(g_y_image, w, h, c) * get_pixel(g_y_image, w, h, c));
+                angle = atan(get_pixel(g_y_image, w, h, c) / get_pixel(g_x_image, w, h, c));
+                set_pixel(mag, w, h, c, val);
+                set_pixel(ang, w, h, c, angle);
+
+            }
+        }
+    }
+    return mag;
 }
--- try.py
+++ try.py
@@ -13,19 +13,23 @@ blur = convolve_image(im, f, 1)
 save_image(blur, "dog-box7")
 
 im = load_image("data/dog.jpg")
-f = make_highpass_filter()
-blur = convolve_image(im, f, 1)
-
+h = make_highpass_filter()
+blur = convolve_image(im, h, 1)
+feature_normalize(blur)
 save_image(blur, "dog-highpass")
 
 im = load_image("data/dog.jpg")
-s = make_sharpen_filter();
+s = make_sharpen_filter()
 sharp = convolve_image(im, s, 1)
+feature_normalize(sharp)
 save_image(sharp, "sharp")
+
 
 im = load_image("data/dog.jpg")
 e = make_emboss_filter()
+g = make_gaussian_filter(2)
 blur = convolve_image(im, e, 1)
+feature_normalize(blur)
 save_image(blur, "emboss")
 
 im = load_image("data/dog.jpg")
@@ -34,10 +38,25 @@ blur = convolve_image(im, f, 1)
 save_image(blur, "dog-gauss")
 
 
+im = load_image("data/dog.jpg")
+f = make_gaussian_filter(2)
+lfreq = convolve_image(im, f, 1)
+hfreq = sub_image(im,lfreq)
+reconstruct = add_image(lfreq,hfreq)
+save_image(lfreq, "low-frequency")
+save_image(hfreq, "high-frequency")
+save_image(reconstruct, "reconstruct")
 
+im = load_image("data/forest.jpg")
+res = sobel_image(im)
+mag = res[0]
+angle = res[1]
+feature_normalize(mag)
+feature_normalize(angle)
+save_image(mag, "magnitude")
+save_image(angle, "gradient")
 
-# im = load_image("data/square.jpg")
-# f = make_box_filter(7)
-# blur = convolve_image(im, f, 1)
-# thumb = nn_resize(blur, blur.w//2, blur.h//2)
-# save_image(thumb, "dogthumb")
+im = load_image("data/forest.jpg")
+res = colorize_sobel(im)
+feature_normalize(res)
+save_image(res, "magnitude_color")
